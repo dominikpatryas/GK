61a024e50aa53af6770943cbb2ffb7e564829d3b
Dominik Patryas <dominik.patryas@gmail.com>
2019-03-03 13:40:49 +0100

    ex1

--- src/rysowanie/Transforms2D.java
+++ src/rysowanie/Transforms2D.java
@@ -14,137 +14,125 @@ public class Transforms2D extends JPanel {
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			Graphics2D g2 = (Graphics2D)g;
-			g2.translate(0,0);  // Moves (0,0) to the center of the display.
+			 g2.translate(300, 300);  // Moves (0,0) to the center of the display.
 			int whichTransform = transformSelect.getSelectedIndex(); /////
 			AffineTransform transform = new AffineTransform();
 			AffineTransform at = new AffineTransform();
 			
 			
-			switch(whichTransform)
-			{
-			case 1:
-			{
-				int x = 150; int xpoczatkowe = x;
-				int y = 200;
-				int[] xValues1 =    {x,x, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20,
-						x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x, xpoczatkowe};
-				
-				 int[] yValues1 = {y, y, y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+120,y+120};
-					
-				
-				   Polygon polygon1 = new Polygon(xValues1, yValues1, 20);
-				   g2.drawPolygon(polygon1);
-				   g2.fill(polygon1);
-				   
-			        
-			        
-				   break;
-			}
-			case 2:
-			{
-				int x = 150; int xpoczatkowe = x;
-				int y = -20;
-				int[] xValues1 =    {x,x, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20,
-						x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x, xpoczatkowe};
-				
-				 int[] yValues1 = {y, y, y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+120,y+120};
-					
-				
-				   Polygon polygon1 = new Polygon(xValues1, yValues1, 20);
-				   g2.translate(20, -50);
-			        g2.rotate(Math.PI/4);
-			      
-				   g2.scale(1.5, 1.5);
-				   g2.drawPolygon(polygon1);
-				   g2.fill(polygon1);
-				   
-				   break;
-				   
-				   
-				   
-				   
-				   
-			}
-			
-			case 3:
-			{
-				int x = 150; int xpoczatkowe = x;
-				int y = 400;
-				int[] xValues1 =    {x,x, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20,
-						x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x, xpoczatkowe};
-				
-				 int[] yValues1 = {y, y, y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+120,y+120};
-					
-				
-				 Polygon polygon1 = new Polygon(xValues1, yValues1, 20);
-				 
-			     g2.scale(1, 2);
-				 g2.rotate(Math.toRadians(180),300,300);
-				 g2.drawPolygon(polygon1);
-				 g2.fill(polygon1);
-				   
-				   break;
-				
-			}
-			case 4:
-			{
-				int x = 150; int xpoczatkowe = x;
-				int y = 200;
-				int[] xValues1 =    {x,x, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20,
-						x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x, xpoczatkowe};
-				
-				 int[] yValues1 = {y, y, y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+120,y+120};
-					
-				
-				 Polygon polygon1 = new Polygon(xValues1, yValues1, 20);
-				 
-			      
-				 AffineTransform tx2 = new AffineTransform();
-				 	tx2.translate(50, -100);
-			        tx2.shear(0, 0.5);
+			int n=11;       
+            double r = 150,
+            		t=0,
+            		k=(Math.PI*2)/n;     
+          
+            int[] x1 = new int[n];
+            int[] y1 = new int[n];
+            
+            int[] x2 = new int[n];
+            int[] y2 = new int[n];
+            
+            for (int i=0;i<n;i++)
+            {
+                x1[i]= (int) (r*Math.sin(t));
+                y1[i]= (int) (r*Math.cos(t));
+                t+=k;
+            }
+            
+            Polygon p1=new Polygon(x1,y1,n);
+            
+            for (int i=0;i<n;i++)
+            {
+            	x2[i] = (int) (r * Math.sin(t));
+                y2[i] = (int) (r * Math.cos(t));
+                
+                if(y2[i]>50)
+                    x2[i]+=100;
+                
+                if(y2[i] < -50)
+                    x2[i]-=100;
+                
+                t+=k;
+            }
+            
+            Polygon p2 = new Polygon(x2,y2,n);
+            
+            switch (whichTransform) {
+                case 0:
+                {
+                    g2.drawPolygon(p1);   
+                    g2.fill(p1);          
+                    break;
+                }
+                case 1:
+                {
+                    g2.scale(0.5,0.5);  
+                    g2.drawPolygon(p1);
+                    g2.fill(p1);
+                    break;
+                }
+                case 2:
+                {
+                    g2.scale(1.5,1.5);
+                    g2.rotate(Math.toRadians(45));  
+                    g2.drawPolygon(p1);
+                    g2.fill(p1);
+                    break;
+                }
+                case 3:
+                {
+                    g2.rotate(Math.toRadians(180));
+                    g2.drawPolygon(p1);
+                    g2.fill(p1);
+                    break;
+                }
+                case 4:
+                {
+                    g2.draw(p2);
+                    g2.fill(p2);
+                    break;
+                }
+                case 5:
+                {
+                    g2.translate(0,-200);
+                    g2.scale(1,0.5);
+                    g2.draw(p1);
+                    g2.fill(p1);
+                    break;
+                }
+                case 6:
+                {
+                    g2.rotate(Math.toRadians(90));
+                    g2.draw(p2);
+                    g2.fill(p2);
+                    break;
+                }
+                case 7:
+                {
+                    g2.rotate(Math.toRadians(180));
+                    g2.drawPolygon(p1);
+                    g2.fill(p1);
+                    break;
+                }
+                case 8:
+                {
+                    g2.translate(-100,100);     
+                    g2.rotate(Math.toRadians(-45));
+                    g2.draw(p1);
+                    g2.fill(p1);
+                    break;
+                }
+                case 9:
+                {
+                    g2.rotate(Math.toRadians(180));
+                    g2.draw(p2);
+                    g2.fill(p2);
+                    break;
+                }
+            }
 
-			        g2.setTransform(tx2);
-			
-				 g2.drawPolygon(polygon1);
-				 g2.fill(polygon1);
-				   
-				   break;
-				
-			}
-			case 5:
-			{
-				int x = 150; int xpoczatkowe = x;
-				int y = 0;
-				int[] xValues1 =    {x,x, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x += 20,
-						x += 20, x += 20, x += 20, x += 20, x += 20, x += 20, x, xpoczatkowe};
-				
-				 int[] yValues1 = {y, y, y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+20, y,y+120,y+120};
-					
-				
-				   Polygon polygon1 = new Polygon(xValues1, yValues1, 20);
-				   g2.drawPolygon(polygon1);
-				   g2.fill(polygon1);
-				   
-			        			        
-				   break;
-				
-			}
-			case 6:
-			{
-				
-				
-			}
-			}
-			
-				
-			
-			// TODO Apply transforms here, depending on the value of whichTransform!
-
-			//g2.drawImage(pic, -200, -150, null); // Draw image with center at (0,0).
-			
-			
-		}
-	}
+            
+        }
+    }
 
 	private Display display;
 	private BufferedImage pic;
@@ -186,7 +174,7 @@ public class Transforms2D extends JPanel {
 		window.setResizable(false);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-		window.setLocation( (screen.width - window.getWidth())/2, (screen.height - window.getHeight())/2 );
+		window.setLocation( (screen.width - window.getWidth())/2, (screen.height - window.getHeight())/2);
 		window.setVisible(true);
 	}
 
